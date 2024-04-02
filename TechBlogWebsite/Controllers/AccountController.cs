e0b6ada4ba1327d9bd565b0b7d6ad785a6c272ec
Khai Nguyen <cuong.0906140073@gmail.com>
2024-04-02 22:49:36 +0700

    Complete basic Register

--- TechBlogWebsite/Controllers/AccountController.cs
+++ TechBlogWebsite/Controllers/AccountController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using TechBlogWebsite.Models;
 
 namespace TechBlogWebsite.Controllers
 {
@@ -13,9 +16,60 @@ namespace TechBlogWebsite.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User _user)
+        {
+            TechBlogDBNetEntities _db = new TechBlogDBNetEntities();
+            if (ModelState.IsValid)
+            {
+                var check = _db.Users.FirstOrDefault(s => s.Email == _user.Email);
+                if (check == null)
+                {
+                    _user.Password = GetMD5(_user.Password);
+                    _user.Avatar = "";
+                    _user.RoleID = 1;
+
+                    _user.IsActive = true;
+                    _user.Meta = "/users/" + _user.FirstName + _user.LastName;
+                    _user.Hide = false;
+                    _db.Configuration.ValidateOnSaveEnabled = false;
+                    _db.Users.Add(_user);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index", "Default");
+                }
+                else
+                {
+                    ViewBag.error = "Email already exists";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+
         public ActionResult Login()
         {
             return View();
         }
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
     }
 }
---
+++ TechBlogWebsite/ViewModels/RegisterVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TechBlogWebsite.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Username can not be blank !!!")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password can not be blank !!!")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Password can not be blank !!!")]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        [NotMapped]
+
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Email can not be blank !!!")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "First Name can not be blank !!!")]
+        public string firstName { get; set; }
+        [Required(ErrorMessage = "Last Name can not be blank !!!")]
+        public string lastName { get; set; }
+    }
+}
